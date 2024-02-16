b4b7880d248a3257722b4d79f7a7f1884507747b
Xinli WU <ryan7gm@gmail.com>
2024-02-17 04:07:37 +1100

    cleanup

--- src/openai/chat.js
+++ src/openai/chat.js
@@ -2,17 +2,6 @@ const OpenAI = require('openai');
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-const createChatCompletion = ({ model, messages, stream = true }) =>
-  openai.chat.completions.create({ model, messages, stream });
-
-const createCompletion = (prompt, { model = 'text-curie-001' }) =>
-  openai.createCompletion({
-    model,
-    prompt,
-  });
-
 module.exports = {
-  createChatCompletion,
-  createCompletion,
   openai,
 };
--- src/openai/image.js
+++ src/openai/image.js
@@ -2,16 +2,6 @@ const OpenAI = require('openai');
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-const createImage = ({ prompt, model = 'dall-e-2', size = '256x256', format }) =>
-  openai.createImage({
-    model: 'dall-e-3',
-    prompt: 'a white siamese cat',
-    n: 1,
-    size: '1024x1024',
-    response_format: 'b64_json',
-  });
-
 module.exports = {
-  createImage,
   openai,
 };
