a66fd53299018cd1ad5e64aae01ef39eacf626be
Xinli WU <ryan7gm@gmail.com>
2024-07-28 22:33:58 +1000

    cleanup

--- src/middleware/disposableEmailBlocker.js
+++ src/middleware/disposableEmailBlocker.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
-import { readFile } from 'node:fs/promises';
 
-const BLOCK_LIST_URL =
-  'https://raw.githubusercontent.com/disposable-email-domains/disposable-email-domains/master/disposable_email_blocklist.conf';
+const BLOCK_LIST_URL = [
+  'https://raw.githubusercontent.com/disposable-email-domains/disposable-email-domains/master/disposable_email_blocklist.conf',
+  'https://raw.githubusercontent.com/xinli-wu/api.chat.xwu.us/main/src/assets/email.block.json'
+];
 
 const disposableEmailBlocker = async (req, res, next) => {
-  const localBlockList = JSON.parse(await readFile('../assets/email.block.json', 'utf8'));
 
   let { email } = req.body;
   email = email.toLowerCase();
-  const { data } = (await axios(BLOCK_LIST_URL, { responseType: 'text' })) || {};
+  const remoteBlockList = await Promise.allSettled(BLOCK_LIST_URL.map(l => axios(l, { responseType: 'text' })));
 
-  const blockList = [...data.split('\n'), ...localBlockList];
+  const blockList = remoteBlockList.map(x => x.value?.data?.split('\n')).flat();
 
   if (blockList.includes(email.split('@')[1])) {
     return res.status(403).send({
